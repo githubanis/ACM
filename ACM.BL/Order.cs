2d493d1168b4b24c81fedc05a90b072ecc686bbc
anis_gitlab <anisurrahman.bs23@gmail.com>
2020-10-21 10:43:50 +0600

    Added Order and Product Repository and tests

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -19,18 +19,12 @@ namespace ACM.BL
         public int OrderId { get; private set; }
         public DateTimeOffset? OrderDate { get; set; }
 
-        public Order Retrieve(int orderId)
-        {
-            return new Order();
-        }
+        
         public List<Order> Retrieve()
         {
             return new List<Order>();
         }
-        public bool Save()
-        {
-            return true;
-        }
+        
         public bool Validate()
         {
             var isValid = true;
--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -21,18 +21,12 @@ namespace ACM.BL
         public string ProductName { get; set; }
         public decimal? CurrentPrice { get; set; }
 
-        public Product Retrieve(int prodictId)
-        {
-            return new Customer();
-        }
+        
         public List<Product> Retrieve()
         {
-            return new List<Customer>();
+            return new List<Product>();
         }
-        public bool Save()
-        {
-            return true;
-        }
+       
         public bool Validate()
         {
             var isValid = true;
